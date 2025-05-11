a53cbad4d28c52f907540be519cd5e6886d78e09
Surroundyou <letthelightsurroundyou@gmail.com>
2025-05-11 15:37:37 +0900

    2

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "coffee-roulette-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./og_icon.png",
+  "./manifest.json",
+  "./main.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
